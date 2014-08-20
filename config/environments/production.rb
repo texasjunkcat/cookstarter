f5698230a62ec484c43847a3e705246c7f739a8a
Tim Graney <texasjunkcat@gmail.com>
2014-08-20 19:20:58 -0400

    set up devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'http://cookstarter.herokuapp.com/' }
+  # Defaul URL for Devise with Heroku
+
+
 end
